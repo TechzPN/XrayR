5ca2e22b4e1e8683909acd8de5a4feb327e8ec45
TechzPN <132004718+TechzPN@users.noreply.github.com>
2023-08-04 10:09:22 +0700

    Update techztrojan.sh

--- techztrojan.sh
+++ techztrojan.sh
@@ -1,188 +1,126 @@
-#!/binstall
+#!/bin/bash
+# bản chạy 443 và 80 xrayr thường
+bash <(curl -ls https://raw.githubusercontent.com/Vankhanh1996/vankhanh1/main/xrayrthuong.sh)
 
-# azz
-red() {
-	echo -e "\033[31m\033[01m$1\033[0m"
-}
 
-green() {
-	echo -e "\033[32m\033[01m$1\033[0m"
-}
+    read -p "Nhập Node ID port 80 :" node_80
+    echo -e "Node_80 là : ${node_80}"
 
-yellow() {
-	echo -e "\033[33m\033[01m$1\033[0m"
-}
+    read -p "Nhập Node ID port 443 :" node_443
+    echo -e "Node_443 là : ${node_443}"    
 
-# azz
-REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
-RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Alpine")
-PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
-PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "apk add -f")
-PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove")
+    read -p "Nhập SpeedLimit :" SpeedLimit
+    echo -e "SpeedLimit = ${SpeedLimit}"
 
-CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
+    read -p "Nhập DeviceLimit :" DeviceLimit
+    echo -e "DeviceLimit = ${DeviceLimit}"
 
-for i in "${CMD[@]}"; do
-	SYS="$i" && [[ -n $SYS ]] && break
-done
+    read -p "Nhập dns trên cloudflare node 443:" CertDomain443
+    echo -e "CertDomain443 = ${CertDomain443}"       
 
-for ((int = 0; int < ${#REGEX[@]}; int++)); do
-	[[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
-done
-
-[[ -z $SYSTEM ]] && red "ẻ e" && exit 1
-
-archAffix() {
-	case "$(uname -m)" in
-  x86_64 | x64 | amd64) return 0 ;;
-	aarch64 | arm64) return 0 ;;
-	*) red "ẻ e k việt sub！" ;;
-	esac
-
-	return 0
-}
-
-install() {
-	install_XrayR
-	clear
-	makeConfig
-}
-
-install_XrayR() {
-	[[ -z $(type -P curl) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} curl
-	[[ -z $(type -P socat) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} socat
-	bash <(curl -Ls https://raw.githubusercontent.com/Vankhanh1996/XrayRCute/master/install.sh)
-}
-
-makeConfig() {
-    echo "------  Config 80 & 443 BY CLOUDFLY.ME ---------"
-	read -p "Loại website của bạn: V2board"
-	echo "---------------"
-	read -p "Link website: https://techzpn.pro/"
-	echo "---------------"
-	read -p "API key của web: admin@techzpn.pro"
-	echo "---------------"
-	read -p "Node ID 80: " NodeID80
-	echo -e "Node 80 là: ${NodeID80}"
-	echo "---------------"
-	read -p "Nhập CertDomain port 80: " CertDomain80
-  echo -e "CertDomain là: ${CertDomain80}"
-  echo "---------------"
-	read -p "Node ID 443: " NodeID443
-	echo -e "Node 80 là: ${NodeID443}"
-	echo "---------------"
-  read -p "Nhập CertDomain port 443: " CertDomain443
-  echo -e "CertDomain là: ${CertDomain443}"
-	echo "---------------"
-  read -p "Nhập limit Device: " limitDevice
-  echo -e "Limit Device là: ${limitDevice}"
-	echo "---------------"
-
-	rm -f /etc/XrayR/config.yml
-	if [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]]; then
-		curl https://get.acme.sh | sh -s email=script@github.com
-		source ~/.bashrc
-		bash ~/.acme.sh/acme.sh --upgrade --auto-upgrade
-	fi
-         cat <<EOF >/etc/XrayR/config.yml
-Log:
-  Level: none 
-  AccessPath: # /etc/XrayR/access.Log
-  ErrorPath: # /etc/XrayR/error.log
-DnsConfigPath: # /etc/XrayR/dns.json
-InboundConfigPath: # /etc/XrayR/custom_inbound.json
-RouteConfigPath: # /etc/XrayR/route.json
-OutboundConfigPath: # /etc/XrayR/custom_outbound.json
-ConnetionConfig:
-  Handshake: 4 
-  ConnIdle: 30 
-  UplinkOnly: 2 
-  DownlinkOnly: 4 
-  BufferSize: 64 
-Nodes:
-  -
-    PanelType: "V2board" 
-    ApiConfig:
-      ApiHost: "https://techzpn.pro"
-      ApiKey: "admin@techzpn.pro"
-      NodeID: $NodeID80
-      NodeType: V2ray 
-      Timeout: 30 
-      EnableVless: false 
-      EnableXTLS: false 
-      SpeedLimit: 0 
-      DeviceLimit: $limitDevice
-      RuleListPath: # /etc/XrayR/rulelist
-    ControllerConfig:
-      DisableSniffing: True
-      ListenIP: 0.0.0.0 
-      SendIP: 0.0.0.0 
-      UpdatePeriodic: 60 
-      EnableDNS: false 
-      DNSType: AsIs 
-      EnableProxyProtocol: false 
-      EnableFallback: false 
-      FallBackConfigs:  
-        -
-          SNI: 
-          Path: 
-          Dest: 80 
-          ProxyProtocolVer: 0 
-      CertConfig:
-        CertMode: http
-        CertDomain: "$CertDomain80" 
-        CertFile: /etc/XrayR/cert/techzpn.crt 
-        KeyFile: /etc/XrayR/cert/techzpn.key
-        Provider: alidns 
-        Email: test@me.com
-        DNSEnv: 
-          ALICLOUD_ACCESS_KEY: aaa
-          ALICLOUD_SECRET_KEY: bbb
-  -
-    PanelType: "V2board" 
-    ApiConfig:
-      ApiHost: "https://techzpn.pro"
-      ApiKey: "admin@techzpn.pro"
-      NodeID: $NodeID443
-      NodeType: Trojan 
-      Timeout: 30 
-      EnableVless: false 
-      EnableXTLS: false 
-      SpeedLimit: 0 
-      DeviceLimit: $limitDevice
-      RuleListPath: # /etc/XrayR/rulelist
-    ControllerConfig:
-      DisableSniffing: True
-      ListenIP: 0.0.0.0 
-      SendIP: 0.0.0.0 
-      UpdatePeriodic: 60 
-      EnableDNS: false 
-      DNSType: AsIs 
-      EnableProxyProtocol: false 
-      EnableFallback: false 
-      FallBackConfigs:  
-        -
-          SNI: 
-          Path: 
-          Dest: 80 
-          ProxyProtocolVer: 0 
-      CertConfig:
-        CertMode: file 
-        CertDomain: "$CertDomain443"
-        CertFile: /etc/XrayR/cert/techzpn.crt 
-        KeyFile: /etc/XrayR/cert/techzpn.key
-        Provider: cloudflare 
-        Email: test@me.com
-        DNSEnv: 
-          CLOUDFLARE_EMAIL: 
-          CLOUDFLARE_API_KEY: 
+        read -p "Nhập dns trên cloudflare node 80:" CertDomain80
+    echo -e "CertDomain443 = ${CertDomain80}"        
+  
+cd /etc/XrayR
 
 EOF
-	cd /etc/XrayR
-	git clone https://github.com/TechzPN/cert.git
-	XrayR restart
-	green "Đã xong, reboot nếu k thành công！"
-	exit 1
-}
-
-install
+cat >config.yml <<EOF
+Log:
+  Level: none # Log level: none, error, warning, info, debug 
+  AccessPath: # /etc/XrayR/access.Log
+  ErrorPath: # /etc/XrayR/error.log
+DnsConfigPath: # /etc/XrayR/dns.json Path to dns config, check https://xtls.github.io/config/base/dns/ for help
+RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
+ConnetionConfig:
+  Handshake: 4 # Handshake time limit, Second
+  ConnIdle: 10 # Connection idle time limit, Second
+  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
+  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
+  BufferSize: 64 # The internal cache size of each connection, kB 
+Nodes:
+  -
+    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
+    ApiConfig:
+      ApiHost: "techzpn.pro"
+      ApiKey: "admin@techzpn.pro"
+      NodeID: $node_80
+      NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
+      Timeout: 30 # Timeout for the api request
+      EnableVless: false # Enable Vless for V2ray Type
+      EnableXTLS: false # Enable XTLS for V2ray and Trojan
+      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: 2 # Local settings will replace remote settings, 0 means disable
+      RuleListPath: /etc/XrayR/AikoBlock # /etc/XrayR/rulelist Path to local rulelist file
+    ControllerConfig:
+      ListenIP: 0.0.0.0 # IP address you want to listen
+      SendIP: 0.0.0.0 # IP address you want to send pacakage
+      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
+      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      DisableUploadTraffic: false # Disable Upload Traffic to the panel
+      DisableGetRule: false # Disable Get Rule from the panel
+      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
+      DisableSniffing: true # Disable domain sniffing 
+      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      EnableFallback: false # Only support for Trojan and Vless
+      FallBackConfigs:  # Support multiple fallbacks
+        -
+          SNI: # TLS SNI(Server Name Indication), Empty for any
+          Path: # HTTP PATH, Empty for any
+          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
+          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+      CertConfig:
+        CertMode: none # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "$CertDomain80" # Domain to cert
+        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/cert/node1.test.com.key
+        Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+        Email: test@me.com
+        DNSEnv: # DNS ENV option used by DNS provider
+          CLOUDFLARE_EMAIL: vkcloudvps.com@gmail.com
+          CLOUDFLARE_API_KEY: c28ba0fcbcf5d4c49d087da9e4cc693280410
+  -
+    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
+    ApiConfig:
+      ApiHost: "techzpn.pro"
+      ApiKey: "admin@techzpn.pro"
+      NodeID: $node_443
+      NodeType: Trojan # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
+      Timeout: 30 # Timeout for the api request
+      EnableVless: false # Enable Vless for V2ray Type
+      EnableXTLS: false # Enable XTLS for V2ray and Trojan
+      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: 2 # Local settings will replace remote settings, 0 means disable
+      RuleListPath: /etc/XrayR/AikoBlock # /etc/XrayR/rulelist Path to local rulelist file
+    ControllerConfig:
+      ListenIP: 0.0.0.0 # IP address you want to listen
+      SendIP: 0.0.0.0 # IP address you want to send pacakage
+      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
+      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      DisableUploadTraffic: false # Disable Upload Traffic to the panel
+      DisableGetRule: false # Disable Get Rule from the panel
+      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
+      DisableSniffing: true # Disable domain sniffing 
+      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      EnableFallback: false # Only support for Trojan and Vless
+      FallBackConfigs:  # Support multiple fallbacks
+        -
+          SNI: # TLS SNI(Server Name Indication), Empty for any
+          Path: # HTTP PATH, Empty for any
+          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
+          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+      CertConfig:
+        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "$CertDomain443" # Domain to cert
+        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/cert/node1.test.com.key
+        Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+        Email: test@me.com
+        DNSEnv: # DNS ENV option used by DNS provider
+          CLOUDFLARE_EMAIL: vkcloudvps.com@gmail.com
+          CLOUDFLARE_API_KEY: c28ba0fcbcf5d4c49d087da9e4cc693280410
+EOF
+cd /root
+xrayr restart
