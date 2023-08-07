e3b7a9fdff3de3d4b3bdea2961c7ae4d6d2e7427
TechzPN <132004718+TechzPN@users.noreply.github.com>
2023-08-07 23:07:52 +0700

    Update and rename 80docker.sh to 443trojan.sh

--- 443trojan.sh
+++ 443trojan.sh
@@ -111,17 +111,18 @@ pre_install_docker_compose() {
     read -p "Nhập Api Key( khoá giao tiếp của web :" APIKEY
     echo -e "API KEY là : ${APIKEY}"
 
-    read -p "Nhập Node ID port 80 :" node_80
-    echo -e "Node_80 là : ${node_80}"
+    read -p "Nhập Node ID port 443 :" node_443
+    echo -e "Node_443 là : ${node_443}"
 
-    read -p "Nhập SpeedLimit :" SpeedLimit
-    echo -e "SpeedLimit = ${SpeedLimit}"
 
-    read -p "giới hạn thiết bị :" DeviceLimit
-    echo -e "DeviceLimit = ${DeviceLimit}"
+    read -p "Nhập subdomain hoặc ip vps vpn cho port443:" CertDomain443
+    echo -e "CertDomain port 443 là = ${CertDomain}"
 
-    read -p "Nhập subdomain hoặc ip vps vpn:" CertDomain
-    echo -e "CertDomain = ${CertDomain}"
+    read -p "Nhập mail cloudflare :" mailcf
+    echo -e "bạn đã nhập mail  là : ${mailcf}"
+
+    read -p "Nhập token cloudflare :" token
+    echo -e "bạn đã nhập token mail  là : ${token}"
 }
 
 # Config docker
@@ -134,7 +135,7 @@ config_docker() {
 version: '3'
 services: 
   xrayr: 
-    image: aikocute/xrayr:v1.3.12
+    image: ghcr.io/xrayr-project/xrayr:latest
     volumes:
       - ./config.yml:/etc/XrayR/config.yml # thư mục cấu hình bản đồ
       - ./dns.json:/etc/XrayR/dns.json 
@@ -159,9 +160,9 @@ Log:
 DnsConfigPath: # ./dns.json Path to dns config
 ConnetionConfig:
   Handshake: 4 # Handshake time limit, Second
-  ConnIdle: 86400 # Connection idle time limit, Second
-  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
-  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
+  ConnIdle: 10 # Connection idle time limit, Second
+  UplinkOnly: 0 # Time limit when the connection downstream is closed, Second
+  DownlinkOnly: 0 # Time limit when the connection is closed after the uplink is closed, Second
   BufferSize: 64 # The internal cache size of each connection, kB
 Nodes:
   -
@@ -169,21 +170,24 @@ Nodes:
     ApiConfig:
       ApiHost: "$domain"
       ApiKey: "$APIKEY"
-      NodeID: $node_80
-      NodeType: V2ray # Node type: V2ray, Shadowsocks, Trojan
-      Timeout: 30 # Timeout for the api request
+      NodeID: $node_443
+      NodeType: Trojan # Node type: V2ray, Shadowsocks, Trojan
+      Timeout: 10 # Timeout for the api request
       EnableVless: false # Enable Vless for V2ray Type
       EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: $SpeedLimit # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: $DeviceLimit # Local settings will replace remote settings, 0 means disable
+      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
       RuleListPath: # ./rulelist Path to local arulelist file
     ControllerConfig:
-      DisableSniffing: true # turn off sniff
       ListenIP: 0.0.0.0 # IP address you want to listen
       SendIP: 0.0.0.0 # IP address you want to send pacakage
-      UpdatePeriodic: 240 # Time to update the nodeinfo, how many sec.
+      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
       EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
       DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      DisableUploadTraffic: false # Disable Upload Traffic to the panel
+      DisableGetRule: false # Disable Get Rule from the panel
+      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
+      DisableSniffing: true # Disable domain sniffing 
       EnableProxyProtocol: false # Only works for WebSocket and TCP
       EnableFallback: false # Only support for Trojan and Vless
       FallBackConfigs:  # Support multiple fallbacks
@@ -193,15 +197,15 @@ Nodes:
           Dest: 80
           ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
       CertConfig:
-        CertMode: none # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "$CertDomain" # Domain to cert
+        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "$CertDomain443" # Domain to cert
         CertFile: ./cert/node1.test.com.cert # Provided if the CertMode is file
         KeyFile: ./cert/node1.test.com.key
         Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
         Email: test@me.com
         DNSEnv: # DNS ENV option used by DNS provider
-          CLOUDFLARE_EMAIL: vvv
-          CLOUDFLARE_API_KEY: 222
+          CLOUDFLARE_EMAIL: vkcloudvps.com@gmail.com
+          CLOUDFLARE_API_KEY: c28ba0fcbcf5d4c49d087da9e4cc693280410
 EOF
 
 }
@@ -331,6 +335,9 @@ Install_xrayr() {
 # Initialization step
 clear
 while true; do
+  echo "phiên bản này được dùng image của xrayr-project đã chỉnh sửa 1 số thống số "
+  echo "fix zalo và chạy đa web lưu ý để tối đa 4 nút cho ổn định"
+  echo "zalo :0968343658"
   echo "Vui lòng nhập một số để Thực Hiện Câu Lệnh:"
   for ((i = 1; i <= ${#operation[@]}; i++)); do
     hint="${operation[$i - 1]}"
